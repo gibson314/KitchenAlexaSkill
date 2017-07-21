61ecf334573756b4a3bd8b778a600855c7814021
Li <zlli@186590d34af1.ant.amazon.com>
2017-07-21 13:06:18 -0700

    Add data used by application

---
+++ data/food_calories.py
@@ -0,0 +1,7 @@
+dishes_calories = {"Pineapple Chicken": 5000, "Potato Beef": 4000, "Onion Lamp": 3000, "Brocoli Pork": 2000}
+food_calories = {"Brocoli": 100, "Pineapple": 200, "Onion": 300, "Potato": 500, "Tomato": 300, "Egg": 300, "Lamp": 2000, "Beef": 3000, "Chicken": 3000, "Pork": 1000}
+food_category = {"Brocoli": "vegetable", "Pineapple": "vegetable", "Onion": "vegetable", "Potato": "vegetable", "Tomato": "vegetable", "Egg": "meat", "Lamp": "meat", "Beef": "meat", "Chicken": "meat", "Pork": "meat"}
+dishes_food = {"Pineapple Chicken":["Pineapple", "Chicken"] , "Potato Beef": ["Potato", "Beef"], "Onion Lamp": ["Onion", "Lamp"], "Brocoli Pork": ["Brocoli", "Pork"]}
+food_unit = {"Brocoli": "lb", "Pineapple": "lb", "Onion": "count", "Potato": "count", "Tomato": "count", "Egg": "count", "Lamp": "lb", "Beef": "lb", "Chicken": "lb", "Pork": "lb"}
+
+
